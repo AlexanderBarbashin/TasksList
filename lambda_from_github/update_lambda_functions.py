907e45a685a49441dc8e8cbac2eff9ca36190f59
angerran <apise41@rambler.ru>
2024-07-22 14:27:16 +0300

    updated buildspec and func to lambda

--- lambda_from_github/update_lambda_functions.py
+++ lambda_from_github/update_lambda_functions.py
@@ -4,18 +4,19 @@ from zipfile import ZipFile
 
 
 def update_lambda_functions(lambda_client):
-    lambda_function_names = environ['LAMBDA_FUNC_NAME']
-    print("Updating lambda functions: " + lambda_function_names[0] + ", " + lambda_function_names[1])
+    lambda_api_service_function_name = environ['LAMBDA_API_SERVICE_FUNC_NAME']
+    lambda_data_service_function_name = environ['LAMBDA_DATA_SERVICE_FUNC_NAME']
+    print("Updating lambda functions: " + lambda_api_service_function_name + ", " + lambda_data_service_function_name)
     with ZipFile('lambda_api_service.zip', 'w') as my_zip:
         my_zip.write('api_service.py')
     with ZipFile('lambda_data_service.zip', 'w') as my_zip:
         my_zip.write('data_service.py')
     lambda_client.update_function_code(
-        FunctionName=lambda_function_names[0],
+        FunctionName=lambda_api_service_function_name,
         ZipFile='lambda_api_service.zip'
     )
     lambda_client.update_function_code(
-        FunctionName=lambda_function_names[1],
+        FunctionName=lambda_data_service_function_name,
         ZipFile='lambda_data_service.zip'
     )
 
