4b932d7a08d5c0807cf993c1e7c06790b3823f07
angerran <apise41@rambler.ru>
2024-08-14 15:49:58 +0300

    updated

---
+++ update_lambda_functions.py
@@ -0,0 +1,23 @@
+import boto3
+from os import environ
+from zipfile import ZipFile
+
+
+def update_lambda_function(lambda_client):
+    lambda_function_name = environ['LAMBDA_FUNC_NAME']
+    lambda_function_dir = environ['LAMBDA_FUNC_DIR']
+    print("Updating lambda function: " + lambda_function_name)
+    with ZipFile('index.zip', 'w') as my_zip:
+        my_zip.write(f'{lambda_function_dir}/index.py')
+    with open('index.zip', 'rb') as zip_file:
+        zip_code = zip_file.read()
+        lambda_client.update_function_code(
+            FunctionName=lambda_function_name,
+            ZipFile=zip_code
+        )
+
+
+if __name__ == "__main__":
+    region = environ['AWS_REGION']
+    client = boto3.client('lambda', region)
+    update_lambda_function(client)
---
+++ update_main_page.py
@@ -0,0 +1,21 @@
+import logging
+
+import boto3
+from os import environ
+from botocore.exceptions import ClientError
+
+
+def update_main_page(s3_client):
+    bucket = environ['S3_BUCKET_NAME']
+    try:
+        response = s3_client.upload_file('web/index.html', bucket)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    region = environ['AWS_REGION']
+    client = boto3.client('s3', region)
+    update_main_page(client)
