81ea617f8fa68d44b9c5dfe40c2c03b305fccf73
angerran <apise41@rambler.ru>
2024-08-14 15:58:44 +0300

    func upload to s3 updated

--- update_main_page.py
+++ update_main_page.py
@@ -8,7 +8,7 @@ from botocore.exceptions import ClientError
 def update_main_page(s3_client):
     bucket = environ['S3_BUCKET_NAME']
     try:
-        response = s3_client.upload_file('web/index.html', bucket)
+        response = s3_client.upload_file('web/index.html', bucket, 'index.html')
     except ClientError as e:
         logging.error(e)
         return False
